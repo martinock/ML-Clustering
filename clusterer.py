6dab5ad31c7890c263cbdb9bd04cb57420a2f02f
13514104 <fairuz.pratama@tokopedia.com>
2017-11-24 18:28:05 +0700

    Changed to procedural form

--- clusterer.py
+++ clusterer.py
@@ -2,57 +2,68 @@ import pandas as pd
 import numpy as np
 import pickle
 from scipy.spatial.distance import squareform, pdist
-from pathlib import Path
 from graph import Graph
 
+data_source = "CensusIncome\CencusIncome.data.txt"
+full_header = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class"]
+numeric_header = ["age", "education-num", "capital-gain", "capital-loss", "hours-per-week"]
+statistic_file = 'data_stat.pkl'
 
-class Clusterer:
 
-    data_source = "CensusIncome\CencusIncome.data.txt"
-    full_header = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class"]
-    numeric_header = ["age", "education-num", "capital-gain", "capital-loss", "hours-per-week"]
-    statistic_file = 'data_stat.pkl'
+def read_numeric_training_data():
+    training_data = pd.read_csv(data_source, header=None, index_col=False, names=full_header)
+    training_data.replace('?', np.NaN)
+    training_data = training_data[numeric_header + ["class"]]
+    training_data = training_data.drop_duplicates(subset=numeric_header, keep='first')
+    training_data.dropna(axis=0, how='any')
+    return training_data
 
-    @staticmethod
-    def read_numeric_training_data():
-        training_data = pd.read_csv(Clusterer.data_source, header=None, index_col=False, names=Clusterer.full_header)
-        training_data.replace('?', np.NaN)
-        training_data = training_data[Clusterer.numeric_header + ["class"]]
-        training_data = training_data.drop_duplicates(subset=Clusterer.numeric_header, keep='first')
-        training_data.dropna(axis=0, how='any')
-        return training_data
 
-    def __init__(self, isload):
-        '''
-        Initiate the data means, standard deviation, and distance matrix
-        :param isload: Whether or not generate the statistic from scratch or to load it from the data
-        '''
-        if not isload:
-            training_data = Clusterer.read_numeric_training_data()
-            self.means = training_data.mean()
-            self.sdts = training_data.std()
-            for coll in Clusterer.numeric_header:
-                training_data[coll] = (training_data[coll] - self.means[coll]) / self.sdts[coll]
-            self.dist_matrix = pd.DataFrame(squareform(pdist(training_data[Clusterer.numeric_header].iloc[:, 1:])))
-            self.save_stat()
-        else:
-            self.load_stat()
+def standardize_data(training_data):
+    means = training_data.mean()
+    sdts = training_data.std()
+    for coll in numeric_header:
+        training_data[coll] = (training_data[coll] - means[coll]) / sdts[coll]
+    return training_data, means, sdts
 
-    def load_stat(self):
-        with open(self.statistic_file, 'rb') as input:
-            data = pickle.load(input)
-            self.means = data[0]
-            self.sdts = data[1]
-            self.dist_matrix = data[2]
 
-    def save_stat(self):
-        with open(self.statistic_file, 'wb') as output:
-            pickle.dump([self.means, self.sdts, self.dist_matrix], output, pickle.HIGHEST_PROTOCOL)
+def generate_dist_matrix(training_data):
+    dist_matrix = pd.DataFrame(squareform(pdist(training_data[numeric_header].iloc[:, 1:])))
+    return dist_matrix
+
+
+def generate_dist_tree(dist_matrix):
+    g = Graph(len(dist_matrix))
+    for i in range(len(dist_matrix)):
+        for j in range(i+1, len(dist_matrix)):
+            g.set_dist(i,j,dist_matrix[i][j])
+    g.gen_min_spanning_tree()
+    return g
+
+
+def save_stat(means, sdts, dist_matrix):
+    with open(statistic_file, 'wb') as output:
+        pickle.dump([means, sdts, dist_matrix], output, pickle.HIGHEST_PROTOCOL)
+
+
+def load_stat():
+    with open(statistic_file, 'rb') as input:
+        data = pickle.load(input)
+        means = data[0]
+        sdts = data[1]
+        dist_matrix = data[2]
+        return means, sdts, dist_matrix
 
 
 def main():
-    #c = Clusterer(False)
-    c = Clusterer(True)
+    data = read_numeric_training_data()
+    data = data.sample(frac=0.01, random_state=17)
+
+    data, means, sdts = standardize_data(data)
+    dist = generate_dist_matrix(data)
+    tree = generate_dist_tree(dist)
+    print(tree)
+
 
 if __name__ == "__main__":
     main()
--- graph.py
+++ graph.py
@@ -12,25 +12,13 @@ class Graph:
         for n, nadjs in self.data_graph.adj.items():
             for nadj, eattrs in nadjs.items():
                 self.data_graph[n][nadj]['weight'] = 0.0
-        self.tree_graph = None
 
-    def dist_str(self):
+    def __str__(self):
         """
         :return: String representation of the distance between data nodes
         """
         out_str = ''
         for n, nadjs in self.data_graph.adj.items():
-            for nadj, eattrs in nadjs.items():
-                out_str += '(%s,%s):%3.2f\t' % (n, nadj, eattrs['weight'])
-            out_str += "\n"
-        return out_str
-
-    def tree_str(self):
-        """
-        :return: String representation of the distance between data nodes
-        """
-        out_str = ''
-        for n, nadjs in self.tree_graph.adj.items():
             for nadj, eattrs in nadjs.items():
                 out_str += '(%s,%s):%3.2f\t' % (n, nadj, eattrs['weight'])
             out_str += "\n"
@@ -51,32 +39,30 @@ class Graph:
         Calculate the spanning tree of the data graph, and save the resulting tree in tree graph
         """
         # TODO: Create own implementation of this
-        self.tree_graph = nx.minimum_spanning_tree(self.data_graph)
+        self.data_graph = nx.minimum_spanning_tree(self.data_graph)
 
     def cut_longest_conn(self, num=0):
         """
         Recursively cut the edges with the longest distance in the tree graph, generate the tree graph if not generated
         :param num: The number of connection to be cut
         """
-        if self.tree_graph == None:
-            self.gen_min_spanning_tree()
         maxs, edges = [], []
         for _ in range(num):
             maxs.append(0.0); edges.append((-1,-1));
-        for n, nadjs in self.tree_graph.adj.items():
+        for n, nadjs in self.data_graph.adj.items():
             for nadj, eattrs in nadjs.items():
                 if min(maxs) < eattrs['weight']:
                     idx = maxs.index(min(maxs))
                     maxs[idx], edges[idx]= eattrs['weight'], (n, nadj)
-        self.tree_graph.remove_edges_from(edges)
+        self.data_graph.remove_edges_from(edges)
 
     def get_connected_tree(self):
         """
         :return: A set of set of connected nodes in the tree graph
         """
-        if self.tree_graph == None:
+        if self.data_graph == None:
             self.gen_min_spanning_tree()
-        return [n for n in nx.connected_components(self.tree_graph)]
+        return [n for n in nx.connected_components(self.data_graph)]
 
 
 def main():
